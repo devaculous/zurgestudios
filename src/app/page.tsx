64a53680fa46c157aa5ae481b4a4dd20fa3c74cb
devaculous <devaculous@gmail.com>
2026-08-07 19:03:00 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import GameShowcase from "@/components/GameShowcase";
-import Contact from "@/components/Contact";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import GameShowcase from "../components/GameShowcase";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
